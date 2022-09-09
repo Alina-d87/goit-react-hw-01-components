5c8af3295f73467b9e0a9f73c05065ca49cbdbb8
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-09-09 22:39:03 +0300

    add faile work

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,13 @@
+/*<ul class="friend-list">
+  <!-- Довільна кіл-сть FriendListItem -->
+</ul>
+
+<li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+
+avatar - посилання на аватар
+name - ім'я друга
+isOnline - буль, що сигналізує про стан друга: в мережі або ні.*/
